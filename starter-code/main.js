a23ee2c1e7fdd67cadb99e6b196805e43711aafd
Dan <you@example.com>
2017-02-21 20:54:02 -0600

    changed cards to Jscript

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,26 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+/*if (cardOne === cardTwo) {
+	alert("You've found a match!");
+} else if ( cardThree === cardFour) {
+	alert("You've found a match!"); 
+} else {
+	alert("Sorry try again!");
+}*/
+
+var createCards = function() {
+
+
+	var gameBoard = document.getElementById('game-board');
+
+	for (var i = 0; i < 4; i++) {
+		var boardCard = document.createElement('div');
+		boardCard.className = 'card';
+		gameBoard.appendChild(boardCard);
+
+	}
+
+}
+
+createCards();
