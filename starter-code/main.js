67404c78bd739327aa99bea94cb9483be039c03a
Dan <you@example.com>
2017-02-22 16:20:30 -0600

    work in progress but submitting anyway

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,17 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/*if (cardOne === cardTwo) {
-	alert("You've found a match!");
-} else if ( cardThree === cardFour) {
-	alert("You've found a match!"); 
-} else {
-	alert("Sorry try again!");
-}*/
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 
 var createCards = function() {
 
@@ -28,3 +18,45 @@ var createCards = function() {
 }
 
 createCards();
+
+/* unsure how to proceed with .setAttribute*/
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		cardElement.setAttribute('data-card', cards[i]);
+		if ('data-card' < 2){
+			'data-card' === "queen"
+		} else {
+			'data-card' === "king"
+		}
+	}
+
+	for (i = 0; i < cards.length; i++) {
+		cardElement.addEventListner('click', isTwoCards);
+		cards[2, 3].innerHTML = '<img src="king.png" alt="King of Diamonds" />';
+		cards[0, 1].innerHTML = '<img src="queen.png" alt="Queen of Spades" />';
+	}
+}
+
+createBoard();
+
+var isMatch = function() {
+	if (cards[0] === cards[1]) {
+		alert("You've found a match!");
+	} else if ( cards[2] === cards[3]) {
+		alert("You've found a match!"); 
+	} else {
+		alert("Sorry try again!");
+	}
+}
+
+isMatch();
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+isTwoCards();
